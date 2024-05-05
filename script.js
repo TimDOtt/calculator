1e5ff5b900a62d3bad0316003c45f4542ca83fac
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-05-05 17:15:21 -0500

    Create calculator functions

---
+++ script.js
@@ -0,0 +1,29 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (arr) {
+  return arr.reduce((value, a) => value + a, 0);
+};
+
+const multiply = function (arr) {
+  return arr.reduce((value, a) => value * a);
+};
+
+const power = function (a, b) {
+  return a ** b;
+};
+
+const factorial = function (a) {
+  if (a < 0) {
+    return -1;
+  } else if (a === 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+};
